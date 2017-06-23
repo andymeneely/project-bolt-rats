7eff26e73bcdd9676ef0ce6c3759dc4ae611b7ed
Andy Meneely <andy@se.rit.edu>
2017-06-22 23:59:04 -0400

    cramming in the info any way i can

--- deck.rb
+++ deck.rb
@@ -2,13 +2,24 @@ require 'squib'
 
 data = Squib.csv file: 'game.csv'
 
-Squib::Deck.new(cards: 7) do
+Squib::Deck.new(cards: 14) do
   use_layout file: ['fantasy.yml', 'layout.yml']
   background color: :white
   rect layout: :cut
   rect layout: :safe
-  # require 'irb'; binding.irb
   text layout: :title, str: data.name
-  text layout: :ur, str: data.size
+  text layout: :type, str: data.size.map { |s| "#{s} Creature"} , font_size: 20
+
+  composition = data.fabric.zip(data.metal, data.oil).map do |f,m,o|
+    str = ""
+    str << "#{f}F " unless f == 0
+    str << "#{m}M " unless m == 0
+    str << "#{o}O " unless o == 0
+    str
+  end
+  text layout: :tr, font_size: 20, str: composition, align: :right
+
+
+
   save format: :png
 end
