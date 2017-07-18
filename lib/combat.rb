40d239c64502cb6539c7c77128237bfb1f69272e
Andy Meneely <andy@se.rit.edu>
2017-07-18 10:18:29 -0400

    sim: implemented exploding dice

--- lib/combat.rb
+++ lib/combat.rb
@@ -7,8 +7,29 @@ class Combat
     @rng = rng
   end
 
-  def roll
-    @rng.next
+  def attack
+    roll = @rng.next(5) + 1
+    case roll
+    when 1
+      return 0
+    when 2
+      return roll
+    when 3
+      return roll
+    when 4
+      return roll
+    when 5
+      return roll
+    when 6
+      next_attack = attack()
+      if next_attack == 0
+        return 0
+      else
+        return roll + next_attack
+      end
+    else
+      $stderr.puts "ERROR! Invalid roll."
+    end
   end
 
 end
--- spec/combat_spec.rb
+++ spec/combat_spec.rb
@@ -4,24 +4,49 @@ describe Combat do
 
   let(:rng) { double("rng") }
 
+  it 'rolls a miss' do
+    expect(rng).to receive(:next).and_return(0) # Roll 1: 0%6 + 1 = 1
+    expect(Combat.new(rng).attack).to eq 0
+  end
+
   it 'returns normal for 2' do
-    expect(rng).to receive(:next).and_return(2)
-    expect(Combat.new(rng).roll).to eq 2
+    expect(rng).to receive(:next).and_return(1) # Roll 2
+    expect(Combat.new(rng).attack).to eq 2
   end
 
   it 'returns normal for 3' do
-    expect(rng).to receive(:next).and_return(3)
-    expect(Combat.new(rng).roll).to eq 3
+    expect(rng).to receive(:next).and_return(2) # Roll 3
+    expect(Combat.new(rng).attack).to eq 3
   end
 
   it 'returns normal for 4' do
-    expect(rng).to receive(:next).and_return(4)
-    expect(Combat.new(rng).roll).to eq 4
+    expect(rng).to receive(:next).and_return(3) # Roll 4
+    expect(Combat.new(rng).attack).to eq 4
   end
 
   it 'returns normal for 5' do
-    expect(rng).to receive(:next).and_return(5)
-    expect(Combat.new(rng).roll).to eq 5
+    expect(rng).to receive(:next).and_return(4) # Roll 5
+    expect(Combat.new(rng).attack).to eq 5
+  end
+
+  it 'handles a crit then number' do
+    expect(rng).to receive(:next).and_return(5) # Roll 6
+    expect(rng).to receive(:next).and_return(2) # Roll 3
+    expect(Combat.new(rng).attack).to eq 9
+  end
+
+  it 'handles a crit, then crit, then number' do
+    expect(rng).to receive(:next).and_return(5) # Roll 6
+    expect(rng).to receive(:next).and_return(5) # Roll 6
+    expect(rng).to receive(:next).and_return(2) # Roll 3
+    expect(Combat.new(rng).attack).to eq 15
+  end
+
+  it 'handles a crit, then crit, then miss' do
+    expect(rng).to receive(:next).and_return(5) # Roll 6
+    expect(rng).to receive(:next).and_return(5) # Roll 6
+    expect(rng).to receive(:next).and_return(0) # Roll 1
+    expect(Combat.new(rng).attack).to eq 0
   end
 
 
