5df6c14fa5c45a63c5830899419c5fba40172deb
Andy Meneely <andy@se.rit.edu>
2017-07-18 01:16:25 -0400

    some cleanup and other ideas immediately after playtest
    Ok NOW I'm gonna go to bed...

--- deck.rb
+++ deck.rb
@@ -17,16 +17,15 @@ Squib::Deck.new(cards: 14) do
 
   composition = data.fabric.zip(data.metal, data.oil).map do |f,m,o|
     str = ""
-    str << "#{f}🏴 " unless f.to_i == 0
     str << "#{m}⚙️ " unless m.to_i == 0
+    str << "#{f}🏴 " unless f.to_i == 0
     str << "#{o}💧 "  unless o.to_i == 0
     str
   end
   text layout: :tr, str: composition,
-       font: 'Sans, Segoe UI Symbol, Dingbats Bold 28' , align: :right, x: 425, width: 325
+       font: 'Sans, Segoe UI Symbol, Dingbats Bold 32' , align: :right, x: 425, width: 325
 
   text layout: :barter, str: data.barter
-
   text layout: :barter, str: 'Barter', valign: :top
 
   text layout: :build, str: data.build
@@ -52,7 +51,6 @@ Squib::Deck.new(cards: 2) do
   item_bonuses = data.recipe.zip(data.bonus).map do |r,b|
     "<span size='60000' rise='-10000'>▢</span><b>#{r}</b>. <i>#{b}</i>"
   end.join("\n\n")
-  puts item_bonuses
   text range: 1, font: 'Archivo Narrow, Sans 29', markup: true,
        spacing: '-5',
        x: 75, y: 75, width: 625, ellipsize: false,
