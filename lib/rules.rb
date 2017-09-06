d376c046693da45476a8a302106c6b6426305c5a
Andy Meneely <andy@se.rit.edu>
2017-09-05 23:22:09 -0400

    Revert "rules: attempt to put into one sheet"
    This reverts commit a286e4ac4864cbaa59f8ed2887e5dab949ab8efd.

--- lib/rules.rb
+++ lib/rules.rb
@@ -6,27 +6,12 @@ rules_text = markdown_to_pdfstyles(File.read('docs/rules.md'))
 rules_text << "\nVersion #{BoltRats::VERSION}"
 
 Prawn::Document.generate("_output/rules_via_prawn.pdf",
+                         page_size: [180, 252], # 2.5in x 3.5in
                          margin: 10,
                          print_scaling: :none
   ) do
+
   font "Times-Roman"
   font_size 8
-  3.downto(1).each do |row|
-    0.upto(2).each do |col|
-      bounding_box([col * 180, row * 252 + 20], width: 180, height: 252) do
-        transparent(0.5) { stroke_bounds }
-        bounding_box([10, cursor - 10], :width => 160) do
-          excess = text_box rules_text,
-                              width: 160,
-                              height: 232,
-                              overflow: :truncate,
-                              indent: 10,
-                              inline_format: true
-          rules_text = excess.inject('') { |str,h| str << h[:text]; str }
-          puts rules_text
-        end
-      end
-    end
-  end
-
+  text rules_text, inline_format: true
 end
