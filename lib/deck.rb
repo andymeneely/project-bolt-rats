9fbd54a6c15cdb1d5864d0ad1b4d512289e02b9f
Andy Meneely <andy@se.rit.edu>
2018-08-14 10:39:42 -0400

    Disassemble --> Ditch for The Crusher
    I never documented Disassemble, so I'm going to assume I meant Ditch.
    
    Made other adjustments too

--- lib/deck.rb
+++ lib/deck.rb
@@ -61,21 +61,23 @@ Squib::Deck.new(cards: 3) do
   item_bonuses = data.recipe.zip(data.bonus).map do |r,b|
     "<b>#{r}</b>. <i>#{b}</i> "
   end.join("\n \n")
-  text range: 1, font: 'Archivo Narrow, Sans 11', markup: true,
+  text range: 1, font: 'Archivo Narrow,Sans 10', markup: true,
        spacing: -8,
        x: 150, y: 75, width: 575, ellipsize: false,
        str: item_bonuses
   0.upto(6).each do |n|
     y = n * 125 + 85
     # y += 10 if n == 4 # Tape dispensor adjustment
-    y += 10 if n == 5 # Tote bag adjustment
-    y -= 10 if n == 6 # Whacking plank adjustment
+    y -= 10 if n == 3 # Strong Magnet
+    y -= 20 if n == 4 # Shiny Shiv
+    y -= 40 if n == 5 # Tote bag adjustment
+    y -= 70 if n == 6 # Whacking plank adjustment
     rect x: 75, y: y, width: 65, height: 65,
          radius: 15, range: 1, stroke_width: 4
   end
   text str: "<b>Zeppelin:</b>\n8 Metal, 6 Fabric, 4 Oil, 3 Duct Tape",
-       font: 'Archivo Narrow, Sans 32', range: 1, markup: true,
-       x: 75, y: 950, width: 675, ellipsize: false, align: :center
+       font: 'Archivo Narrow,Sans 12', range: 1, markup: true,
+       x: 75, y: 900, width: 675, ellipsize: false, align: :center
   save_pdf file: 'resources.pdf', trim: 37.5
   save_png prefix: 'resource_'
 end
